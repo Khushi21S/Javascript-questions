4dd4cd1e8ac476bc798e410ee2563ce7ecb1b420
Khushi Sharma <87928896+Khushi21S@users.noreply.github.com>
2023-06-06 10:32:16 +0530

    Add files via upload

---
+++ q8.js
@@ -0,0 +1,15 @@
+
+
+
+const num = Math.ceil(Math.random()*10);
+console.log(num);
+const num2 = prompt('Guess the number between 1 to 10');
+if(num==num2){
+    console.log("Good Work");
+}
+else{
+    console.log("Not matched, The number was "+ num2);
+}
+
+
+
