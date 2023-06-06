0b94565140774bf7753bba3f27ee0884f41bfde8
Khushi Sharma <87928896+Khushi21S@users.noreply.github.com>
2023-06-06 10:16:03 +0530

    Add files via upload

---
+++ q7.js
@@ -0,0 +1,11 @@
+console.log("-----------------");
+for (var year= 2014; year<= 2050; year++){
+    var d = new Date(year, 0, 1);
+    if (d.getDay()===0){
+        console.log("1st Jan is Sunday " + year);
+    }
+}
+console.log("----------------");
+
+
+
